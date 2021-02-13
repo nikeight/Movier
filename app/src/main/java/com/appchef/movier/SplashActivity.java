a9adac90b43c20f7b84e11c1e6e35be366fd52b8
Niket <52085669+nikeight@users.noreply.github.com>
2021-02-13 19:29:42 +0530

    Initial Commit (#3)
    * Initial Commit
    
    * Added splash screen
    
    * optimized code
    
    Co-authored-by: Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>
    Co-authored-by: Pratik Patle <44264689+pratik2550@users.noreply.github.com>

---
+++ app/src/main/java/com/appchef/movier/SplashActivity.java
@@ -0,0 +1,62 @@
+package com.appchef.movier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView splashLogoIv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        init();
+        int number = getRandomNumber();
+        setIcon(number);
+        splashScreenHandler();
+    }
+
+    private void splashScreenHandler() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
+    }
+
+    private void setIcon(int number) {
+        switch (number) {
+            case 1:
+                splashLogoIv.setImageResource(R.drawable.ic_foodcircle);
+                break;
+            case 2:
+                splashLogoIv.setImageResource(R.drawable.ic_musicalnotes);
+                break;
+            default:
+                splashLogoIv.setImageResource(R.drawable.ic_clapperboard);
+        }
+    }
+
+    private int getRandomNumber() {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(3);
+        return randomNumber;
+    }
+
+    private void init() {
+        splashLogoIv = findViewById(R.id.splashLogoIv);
+    }
+}
