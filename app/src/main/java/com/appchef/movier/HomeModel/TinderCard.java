ea4ae722fa8e140a303c6812b882790e1f9dbfa9
Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>
2021-02-14 18:31:37 +0530

    saving the accepted swipe request data.

--- app/src/main/java/com/appchef/movier/HomeModel/TinderCard.java
+++ app/src/main/java/com/appchef/movier/HomeModel/TinderCard.java
@@ -6,8 +6,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
 import com.appchef.movier.R;
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.auth.User;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
 import com.mindorks.placeholderview.annotations.Layout;
@@ -18,6 +25,9 @@ import com.mindorks.placeholderview.annotations.swipe.SwipeIn;
 import com.mindorks.placeholderview.annotations.swipe.SwipeInState;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOutState;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Layout(R.layout.person_home_card_view)
 public class TinderCard {
@@ -77,14 +87,38 @@ public class TinderCard {
         mSwipeView.addView(this);
     }
 
-    @SwipeCancelState
-    public void onSwipeCancelState() {
-        Log.d("EVENT", "onSwipeCancelState");
-    }
-
     @SwipeIn
     public void onSwipeIn() {
         Log.d("EVENT", "onSwipedIn");
+        // Saving the Swipe In Data.
+        Map<String,Object> userData = new HashMap<>();
+        userData.put("username",mUsers.getUsername());
+        userData.put("uniqueID",mUsers.getUniqueID());
+        userData.put("requestStatus",true);
+        userData.put("imageUrl",mUsers.getImageUrl());
+
+        String uniqueID = FirebaseAuth.getInstance().getUid();
+        if (uniqueID != null)
+        FirebaseFirestore.getInstance().collection("users").document(uniqueID)
+                .collection("swipeAccept").add(userData).addOnSuccessListener(
+                new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(mContext, "Request Send", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        ).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.i("TAG",Log.getStackTraceString(e));
+                Toast.makeText(mContext, "The user didn't get your accept request", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @SwipeCancelState
+    public void onSwipeCancelState() {
+        Log.d("EVENT", "onSwipeCancelState");
     }
 
     @SwipeInState
--- app/src/main/java/com/appchef/movier/HomeModel/Users.java
+++ app/src/main/java/com/appchef/movier/HomeModel/Users.java
@@ -2,18 +2,28 @@ package com.appchef.movier.HomeModel;
 
 public class Users {
 
-    String fullName, username, age, gender, imageUrl, contactNumber;
+    String fullName, username, age, gender, imageUrl, contactNumber,uniqueID;
 
     public Users() {
+        // Empty Const. to prevent from crashing.
     }
 
-    public Users(String fullName, String username, String age, String gender, String imageUrl, String contactNumber) {
+    public Users(String fullName, String username, String age, String gender, String imageUrl, String contactNumber, String uniqueID) {
         this.fullName = fullName;
         this.username = username;
         this.age = age;
         this.gender = gender;
         this.imageUrl = imageUrl;
         this.contactNumber = contactNumber;
+        this.uniqueID = uniqueID;
+    }
+
+    public String getUniqueID() {
+        return uniqueID;
+    }
+
+    public void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
     }
 
     public String getFullName() {
