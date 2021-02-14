4f3b90725c65a1b0260bcaa774b9c5f698a5d855
Niket <52085669+nikeight@users.noreply.github.com>
2021-02-14 15:37:52 +0530

    Registration Part (#13)
    * RegistrationFragment Added and improved WorkFlow.
    
    * Merged Login Branch to Registration Branch
    
    Co-authored-by: Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>

--- app/src/main/java/com/appchef/movier/Activity/LoginActivity.java
+++ app/src/main/java/com/appchef/movier/Activity/LoginActivity.java
@@ -108,15 +108,13 @@ public class LoginActivity extends AppCompatActivity {
                             mProgressDialog.dismiss();
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            ForwardToMainScreen();
+                            ForwardToRegistrationScreen();
                         } else {
                             // If sign in fails, display a message to the user.
                             mProgressDialog.dismiss();
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
                             Toast.makeText(LoginActivity.this, "Sign in failure", Toast.LENGTH_SHORT).show();
                         }
-
-                        // ...
                     }
                 });
     }
@@ -137,11 +135,11 @@ public class LoginActivity extends AppCompatActivity {
         // Check if the user is signed in or not.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser!= null){
-            ForwardToMainScreen();
+            ForwardToRegistrationScreen();
         }
     }
 
-    private void ForwardToMainScreen(){
+    private void ForwardToRegistrationScreen(){
         Intent mainIntent = new Intent(LoginActivity.this, RegistrationActivity.class);
         startActivity(mainIntent);
         finish();
